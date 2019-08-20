47b51e4812ebd14364a651c6233fc6f2d232f77a
Milada <greatchocopie@yandex.com>
2019-08-20 17:37:22 +0800

    мобильная версия слайдера update, но на примитивном js, нужно доделывать и сделать доступность

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,12 @@ var faqButtonOpen = faq.querySelector(".faq__item-icon--opened");
 var faqAnswer = faq.querySelector(".faq__item-answer");
 var faqButtonClose = faq.querySelector(".faq__item-icon--closed");
 
+var update = document.querySelector(".update");
+var updateButtonLeft = document.querySelector(".update__slides-list-left--js");
+var updateButtonRight = document.querySelector(".update__slides-list-right--js");
+var updateSliderList1 = document.querySelector(".update__slides-list1");
+var updateSliderList2 = document.querySelector(".update__slides-list2");
+
 faqButtonOpen.addEventListener("click", function (evt) {
   evt.preventDefault();
   faqAnswer.classList.add("faq__item-answer--show");
@@ -16,4 +22,16 @@ faqButtonClose.addEventListener("click", function (evt) {
   faqButtonClose.classList.remove("faq__item-answer--show");
   faqButtonOpen.classList.remove("faq__item-icon--closed");
 
+});
+
+
+updateButtonRight.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  updateSliderList2.classList.remove("update__slides-list-none");
+  updateSliderList1.classList.add("update__slides-list-none");
+});
+updateButtonLeft.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  updateSliderList1.classList.remove("update__slides-list-none");
+  updateSliderList2.classList.add("update__slides-list-none");
 });
